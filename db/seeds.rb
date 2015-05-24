898c60eef206807b55678a2bf567e4e134f049ef
vintang <vintang901@gmail.com>
2015-05-24 19:14:15 +1000

    Seed commit with csv file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+require 'csv'
+
+puts Dir.pwd
+locations = CSV.read('./db/Postcodes.csv')
+
+
+
+for i in 0..locations.size-1
+  
+  if ( locations[i][0].to_i >= 3000 && locations[i][0].to_i <= 3999 )
+    
+    Location.create_without_ref_code(
+      long: locations[i][6],
+      lat: locations[i][5],
+      timezone: "Vic"
+    )
+  end
+
+end
