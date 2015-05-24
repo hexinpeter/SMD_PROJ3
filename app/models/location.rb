55fe94f01ed9af5330f8bfe4dcd1379aa25502bc
Peter He <hexinpeter@gmail.com>
2015-05-24 18:52:15 +1000

    ref_code generation added to location

--- app/models/location.rb
+++ app/models/location.rb
@@ -3,4 +3,22 @@ class Location < ActiveRecord::Base
   has_many :records
 
   delegate :predicted_records, :actual_records, to: :records
+
+  def Location.create_without_ref_code(params={})
+    loc = Location.new(params)
+    loc.ref_code = generate_ref_code
+    loc.save!
+    loc
+  end
+
+  def Location.new_without_ref_code(params={})
+    loc = Location.new(params)
+    loc.ref_code = generate_ref_code
+    loc
+  end
+
+  private
+    def Location.generate_ref_code
+      Location.order(:ref_code).last.ref_code ? Location.order(:ref_code).last.ref_code + 1 : 10000
+    end
 end
